9802b831b4d5ba980b946c63b1117507e6be2160
Jen George Koshy <jengeorgekoshy333@gmail.com>
2022-06-28 20:05:00 +0530

    change in app last line

--- app.py
+++ app.py
@@ -77,9 +77,10 @@ def camera():
     return render_template("index.html",final_output=final_output1,scroll = 'main')
 
 
-
+port = int(os.environ.get("PORT", 5000))
 if __name__ == "__main__":
-    app.run(debug=True)
+        app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
 
